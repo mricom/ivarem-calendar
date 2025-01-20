2cd59a8057372aa6d6b37e0a5b4be307c317e1d6
Maria Rico Morillo <maria.morillo@waterleau.com>
2025-01-20 12:07:01 +0100

    Fixed small things.

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-ZIP_CODE_ID = os.environ("ZIP_CODE_ID")
-STREET_ID = os.environ("STREET_ID")
-HOUSE_NUMBER = os.environ("HOUSE_NUMBER")
-STRAAT = os.environ("STRAAT")
-GEEMENTE = os.environ("GEEMENTE")
+ZIP_CODE_ID = os.environ.get("ZIP_CODE_ID")
+STREET_ID = os.environ.get("STREET_ID")
+HOUSE_NUMBER = os.environ.get("HOUSE_NUMBER")
+STRAAT = os.environ.get("STRAAT")
+GEEMENTE = os.environ.get("GEEMENTE")
